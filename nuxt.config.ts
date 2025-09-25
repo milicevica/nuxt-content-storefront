36bf77e9d7c428f92693a02c310b85fb1a800422
Aleksandar Milicevic <hi@milicevic.dev>
2025-09-25 17:21:56 +0200

    nuxt config for content preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxthq/studio']
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  }
 })

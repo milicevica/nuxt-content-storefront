e40bb9e4180b9060b4de04e426667c0e0e2f9377
Aleksandar Milicevic <hi@milicevic.dev>
2025-09-25 17:04:58 +0200

    add first markdown file and serve it on the '/' route

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { data: page } = await useAsyncData(() => queryCollection('content').path('/').first())
+
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found' , fatal: true })
+}
+</script>
+
+<template>
+  <ContentRenderer v-if="page" :value="page" />
+</template>

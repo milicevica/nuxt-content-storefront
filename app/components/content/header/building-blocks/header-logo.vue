45d18a68c8b1451bbd6bbca6754ee2aa8840c935
Aleksandar Milicevic <hi@milicevic.dev>
2025-09-28 16:45:17 +0200

    simplify logo component

--- app/components/content/header/building-blocks/header-logo.vue
+++ app/components/content/header/building-blocks/header-logo.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="flex items-center justify-center space-x-2">
-    <img
-      src="/logo.png"
-      alt="Storefront Logo"
-      class="h-8 w-32"
-    >
-  </div>
+  <img
+    src="/logo.png"
+    alt="Storefront Logo"
+    class="h-8 w-32"
+  >
 </template>

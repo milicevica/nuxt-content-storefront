0cd672d3cd194b26d69011dcae0079ff27b3bd13
Aleksandar Milicevic <hi@milicevic.dev>
2025-09-28 16:05:00 +0200

    basic footer component

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <footer class="fixed bottom-0 left-0 w-full z-50">
-    This is a footer
+  <footer class="fixed bottom-0 left-0 w-full z-50 py-4">
+    <div class="container mx-auto text-center text-sm">
+      © {{ new Date().getFullYear() }} Your Company. All rights reserved.
+    </div>
   </footer>
 </template>

d426437bfd2b472e8bd609a8458fbe9d35c2ec4c
Aleksandar Milicevic <hi@milicevic.dev>
2025-09-28 16:02:11 +0200

    header content

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,8 +2,10 @@
   <div id="nuxt-content-storefront" class="flex flex-col min-h-screen gap-6">
     <AppHeader />
 
-    <main class="flex-1 pt-16 pb-16">
-      <slot />
+    <main class="flex-1 pt-32 pb-32">
+      <div class="w-4/5 mx-auto p-4">
+        <slot />
+      </div>
     </main>
 
     <AppFooter />

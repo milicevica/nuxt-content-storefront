5f072078b8be884cf424f5178698b932395ac6ee
Aleksandar Milicevic <hi@milicevic.dev>
2025-09-29 13:54:10 +0200

    add 10 default endpoints

--- shared/types/endpoints.ts
+++ shared/types/endpoints.ts
@@ -1,3 +1,12 @@
 export enum Endpoints {
   Categories = "/api/ecommerce/categories",
+  Category = "/api/ecommerce/category",
+  Products = "/api/ecommerce/products",
+  Product = "/api/ecommerce/product",
+  Cart = "/api/ecommerce/cart",
+  User = "/api/ecommerce/user",
+  Checkout = "/api/ecommerce/checkout",
+  Login = "/api/ecommerce/login",
+  Register = "/api/ecommerce/regiseter",
+  Logout = "/api/ecommerce/logout",
 }
